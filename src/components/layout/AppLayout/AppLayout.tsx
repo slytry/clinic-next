9a9035dd3fe23e118e5102572e095a7927d3be6f
Nikita Naumov <naumov99.rst@gmail.com>
2023-01-22 22:22:46 +0300

    feat: comment fonts

--- src/components/layout/AppLayout/AppLayout.tsx
+++ src/components/layout/AppLayout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { IBM_Plex_Sans, Roboto } from '@next/font/google';
+// import { IBM_Plex_Sans, Roboto } from '@next/font/google';
 import Head from 'next/head';
 import { type PropsWithChildren } from 'react';
 
@@ -9,16 +9,18 @@ import { Header } from '../Header';
 
 import s from './AppLayout.module.scss';
 
-const fontIBM = IBM_Plex_Sans({
-	weight: ['400', '500', '600', '700'],
-	variable: '--next-font-IBM',
-	subsets: ['latin', 'cyrillic'],
-});
-const fontRoboto = Roboto({
-	weight: ['400', '500', '700'],
-	variable: '--next-font-Roboto',
-	subsets: ['latin', 'cyrillic'],
-});
+// const fontIBM = IBM_Plex_Sans({
+// 	weight: ['400', '500', '600', '700'],
+// 	variable: '--next-font-IBM',
+// 	subsets: ['latin', 'cyrillic'],
+// });
+// const fontRoboto = Roboto({
+// 	weight: ['400', '500', '700'],
+// 	variable: '--next-font-Roboto',
+// 	subsets: ['latin', 'cyrillic'],
+// });
+
+// fontIBM.variable, fontRoboto.variable
 
 export function AppLayout({
 	children,
@@ -32,7 +34,7 @@ export function AppLayout({
 			<Burger />
 
 			<div
-				className={cn(s.AppLayout, fontIBM.variable, fontRoboto.variable)}
+				className={cn(s.AppLayout)}
 				id="page-wrap"
 				style={{ height: '100%', overflow: 'auto' }}
 			>
